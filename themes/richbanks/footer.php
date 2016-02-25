219d322aacf59957e6012bbf020023b9e3ec61c1
Richard Banks <richard@corbandirect.co.uk>
2016-02-25 15:34:43 +0000

    social on footer

--- themes/richbanks/footer.php
+++ themes/richbanks/footer.php
@@ -1,5 +1,12 @@
 		<footer class="container">
-			<p>If you have any questions, feedback or would just like to get in touch, please email me at &#104;&#101;&#108;&#108;&#111;&#64;&#114;&#105;&#99;&#104;&#98;&#97;&#110;&#107;&#115;&#46;&#99;&#111;&#46;&#117;&#107;</p>
+			<div class="row">
+				<p class="col-sm-10">If you have any questions, feedback or would just like to get in touch, please email me at &#104;&#101;&#108;&#108;&#111;&#64;&#114;&#105;&#99;&#104;&#98;&#97;&#110;&#107;&#115;&#46;&#99;&#111;&#46;&#117;&#107;</p>
+				<p class="col-sm-2 pull-right">
+					<a href="https://twitter.com/richbnaks7"><img src="<?php echo theme_url('img/twitter_icon.svg'); ?>" alt="twitter icon" width="32" height="32"></a>
+					<a href="https://github.com/richbnaks7"><img src="<?php echo theme_url('img/git-hub.svg'); ?>" alt="github icon" width="32" height="32"></a>
+					<a href="/feeds/rss"><img src="<?php echo theme_url('img/rss_feed.svg'); ?>" alt="rss feed icon" width="32" height="32"></a>
+				</p>
+			</div>
 		</footer>
 
 				<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
