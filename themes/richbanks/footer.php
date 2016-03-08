825e2400a5ff50815b3b6fab2a20e63b3f89697d
Richard Banks <richard@corbandirect.co.uk>
2016-03-08 15:21:41 +0000

    flickr

--- themes/richbanks/footer.php
+++ themes/richbanks/footer.php
@@ -3,6 +3,7 @@
 				<p class="col-sm-10">If you have any questions, feedback or would just like to get in touch, please email me at &#104;&#101;&#108;&#108;&#111;&#64;&#114;&#105;&#99;&#104;&#98;&#97;&#110;&#107;&#115;&#46;&#99;&#111;&#46;&#117;&#107;</p>
 				<p class="col-sm-2 pull-right">
 					<a href="https://twitter.com/richbnaks7"><img src="<?php echo theme_url('img/twitter_icon.svg'); ?>" alt="twitter icon" width="32" height="32"></a>
+					<a href="https://www.flickr.com/photos/127172784@N05/"><img src="<?php echo theme_url('img/flickr_icon.svg'); ?>" alt="flickr icon" width="32" height="32"></a>
 					<a href="https://github.com/richbnaks7"><img src="<?php echo theme_url('img/git-hub.svg'); ?>" alt="github icon" width="32" height="32"></a>
 					<a href="/feeds/rss"><img src="<?php echo theme_url('img/rss_feed.svg'); ?>" alt="rss feed icon" width="32" height="32"></a>
 				</p>
