15b64e3645b37e23479cca18c58971ad12cc6632
Richard Banks <richard@corbandirect.co.uk>
2016-03-08 14:07:07 +0000

    404

--- themes/richbanks/build/404.php
+++ themes/richbanks/build/404.php
@@ -1,9 +1,9 @@
 <?php theme_include('header'); ?>
 
-	<section class="content wrap">
+	<section class="container">
 		<h1>Page not found</h1>
 
-		<p>Unfortunately, the page <code>/<?php echo htmlspecialchars(current_url()); ?></code> could not be found. Your best bet is either to try the <a href="<?php echo base_url(); ?>">homepage</a>, try <a href="#search">searching</a>, or go and cry in a corner (although I don’t recommend the latter).</p>
+		<p>Unfortunately, the page <code>/<?php echo htmlspecialchars(current_url()); ?></code> could not be found. Your best bet is either to try the <a href="<?php echo base_url(); ?>">homepage</a> or go and cry in a corner (although I don’t recommend the latter).</p>
 	</section>
 
 <?php theme_include('footer'); ?>
